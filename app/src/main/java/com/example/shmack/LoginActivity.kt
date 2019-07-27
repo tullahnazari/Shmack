3b8e21449a2aa1eca925a45a988a4dc496f3eec6
Mr. Tee <tullah.nazari@gmail.com>
2019-07-27 05:57:56 -0500

    CreateuserNavigationandui
    go to a different page. create finders for elements on the page

--- app/src/main/java/com/example/shmack/LoginActivity.kt
+++ app/src/main/java/com/example/shmack/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.shmack
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -12,6 +13,8 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginCreateUserBtnClicked(view: View) {
+        val createUserIntent = Intent(this, CreateUserActivity::class.java)
+        startActivity(createUserIntent)
 
     }
 
