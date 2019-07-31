ecc190fc8fa17533d6b87e0ea98b65a682ded2ff
Mr. Tee <tullah.nazari@gmail.com>
2019-07-31 17:38:07 -0400

    LoginandauthenticatingUSER
    Login and authenticate a user
    Direct user to home screen
    Clear out fields if authenticated

--- app/src/main/java/com/example/shmack/Controller/LoginActivity.kt
+++ app/src/main/java/com/example/shmack/Controller/LoginActivity.kt
@@ -5,12 +5,32 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.shmack.R
+import com.example.shmack.Services.AuthService
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun loginLoginBtnClicked(view: View) {
+        val email = loginEmailText.text.toString()
+        val password = loginPasswordText.text.toString()
+
+        AuthService.loginUser(this, email, password) {
+                loginSuccess ->
+            if (loginSuccess) {
+                AuthService.findUserByEmail(this) {
+                        findSuccess ->
+                    if (findSuccess) {
+                        finish()
+                    }
+
+                }
+            }
+        }
     }
 
     fun loginCreateUserBtnClicked(view: View) {
@@ -20,9 +40,6 @@ class LoginActivity : AppCompatActivity() {
 
     }
 
-    fun loginLoginBtnClicked(view: View) {
-
-    }
 
 
 }
--- app/src/main/java/com/example/shmack/Services/AuthService.kt
+++ app/src/main/java/com/example/shmack/Services/AuthService.kt
@@ -1,14 +1,14 @@
 package com.example.shmack.Services
 
 import android.content.Context
+import android.content.Intent
 import android.util.Log
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
-import com.example.shmack.Utilities.URL_CREATE_USER
-import com.example.shmack.Utilities.URL_LOGIN
-import com.example.shmack.Utilities.URL_REGISTER
+import com.example.shmack.Utilities.*
 import org.json.JSONException
 import org.json.JSONObject
 import java.lang.reflect.Method
@@ -72,7 +72,7 @@ object AuthService {
                 Response.ErrorListener {
                 //this is where we deal with our our errors
 
-                        error -> Log.d("ERROR", "Couldn't register the user: $error")
+                        error -> Log.d("ERROR", "Couldn't login user: $error")
                     complete(false)
         })  {
 
@@ -132,6 +132,42 @@ object AuthService {
             }
         }
         Volley.newRequestQueue(context).add(createRequest)
+    }
+
+    fun findUserByEmail(context: Context, complete: (Boolean) -> Unit) {
+        val findUserRequest = object: JsonObjectRequest(Method.GET, "$URL_GET_USER$userEmail", null, Response.Listener {response ->
+            try{
+                UserDataService.name = response.getString("name")
+                UserDataService.email = response.getString("email")
+                UserDataService.avatarName = response.getString("avatarName")
+                UserDataService.avatarColor = response.getString("avatarColor")
+                UserDataService.id = response.getString("_id")
+
+                val userDataChange = Intent(BROADCAST_USER_DATA_CHANGE)
+                LocalBroadcastManager.getInstance(context).sendBroadcast(userDataChange)
+                complete(true)
+
+            } catch (e: JSONException) {
+                Log.d("JSON", "EXC: " + e.localizedMessage)
+            }
+
+        }, Response.ErrorListener {
+            Log.d("ERROR", "Couldn't find user")
+            complete(false)
+        }) {
+            override fun getBodyContentType(): String {
+                return "application/json; charset=utf-8"
+            }
+
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers.put("Authorization", "Bearer $authToken")
+                return headers
+            }
+
+        }
+
+        Volley.newRequestQueue(context).add(findUserRequest)
     }
 
 }
