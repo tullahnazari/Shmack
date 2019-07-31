f4cf6a39f61f75933a00c7d0f0934bca2a2dd78c
Mr. Tee <tullah.nazari@gmail.com>
2019-07-31 09:24:56 -0400

    BroadcastReceiverAndUIChanges
    - Ability to add BG, avatar, name, etc
    - Ability to remove above mentioned when logging out
    - Ability navigate to certain activity after button click with 'finish'

--- app/src/main/java/com/example/shmack/Controller/MainActivity.kt
+++ app/src/main/java/com/example/shmack/Controller/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.example.shmack.Controller
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.graphics.Color
 import android.os.Bundle
 import androidx.core.view.GravityCompat
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import android.view.View
+import android.widget.Toast
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.example.shmack.R
+import com.example.shmack.Services.AuthService
+import com.example.shmack.Services.UserDataService
+import com.example.shmack.Utilities.BROADCAST_USER_DATA_CHANGE
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.nav_header_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,6 +38,25 @@ class MainActivity : AppCompatActivity() {
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
 
+        LocalBroadcastManager.getInstance(this).registerReceiver(userDataChangeReceiver,
+            IntentFilter(BROADCAST_USER_DATA_CHANGE)
+        )
+
+    }
+
+    private val userDataChangeReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (AuthService.isLoggedIn) {
+                userNameNavHeader.text = UserDataService.name
+                userEmailNavHeader.text = UserDataService.email
+                val resourceId = resources.getIdentifier(UserDataService.avatarName, "drawable",
+                     packageName)
+                userImageNavHeader.setImageResource(resourceId)
+                userImageNavHeader.setBackgroundColor(UserDataService.returnAvatarColor(UserDataService.avatarColor))
+                loginButtonNavHeader.text = "Logout"
+            }
+        }
+
     }
 
     override fun onBackPressed() {
@@ -41,8 +70,21 @@ class MainActivity : AppCompatActivity() {
     //creating intent to go to loginpage after clicking login button on main page
     fun loginBtnNavClicked(view: View) {
 
-        val loginIntent = Intent(this, LoginActivity::class.java)
-        startActivity(loginIntent)
+        if (AuthService.isLoggedIn) {
+            //log out
+
+            Toast.makeText(this, "You have been logged out", Toast.LENGTH_SHORT).show()
+            UserDataService.logout()
+            userNameNavHeader.text = "Login"
+            userEmailNavHeader.text = ""
+            userImageNavHeader.setImageResource(R.drawable.profiledefault)
+            userImageNavHeader.setBackgroundColor(Color.TRANSPARENT)
+            loginButtonNavHeader.text = "Login"
+
+        } else {
+            val loginIntent = Intent(this, LoginActivity::class.java)
+            startActivity(loginIntent)
+        }
     }
 
     fun addChannelClicked(view: View) {
