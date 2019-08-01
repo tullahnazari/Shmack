db9cbb61a1b9f118e867b8b61fedac01c0622686
Mr. Tee <tullah.nazari@gmail.com>
2019-08-01 05:45:23 -0400

    Finished up create user activity and login activity
    - Finished up create user activity and login activity
    - added progress bar to login screen

--- app/src/main/java/com/example/shmack/Controller/LoginActivity.kt
+++ app/src/main/java/com/example/shmack/Controller/LoginActivity.kt
@@ -1,11 +1,15 @@
 package com.example.shmack.Controller
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import com.example.shmack.R
 import com.example.shmack.Services.AuthService
+import kotlinx.android.synthetic.main.activity_create_user.*
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -13,24 +17,38 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        loginSpinner.visibility = View.INVISIBLE
     }
 
     fun loginLoginBtnClicked(view: View) {
+        enableSpinner(true)
         val email = loginEmailText.text.toString()
         val password = loginPasswordText.text.toString()
+        hideKeyboard()
+        if (email.isNotEmpty() && password.isNotEmpty()) {
 
-        AuthService.loginUser(this, email, password) {
-                loginSuccess ->
-            if (loginSuccess) {
-                AuthService.findUserByEmail(this) {
-                        findSuccess ->
-                    if (findSuccess) {
-                        finish()
+            AuthService.loginUser(this, email, password) {
+                    loginSuccess ->
+                if (loginSuccess) {
+                    AuthService.findUserByEmail(this) {
+                            findSuccess ->
+                        if (findSuccess) {
+                            enableSpinner(false)
+                            finish()
+                        } else {
+                            errorToast()
+                        }
+
                     }
-
+                } else {
+                    errorToast()
                 }
             }
+
+        } else {
+            Toast.makeText(this, "Please fill in both email and password", Toast.LENGTH_LONG).show()
         }
+
     }
 
     fun loginCreateUserBtnClicked(view: View) {
@@ -40,6 +58,33 @@ class LoginActivity : AppCompatActivity() {
 
     }
 
+    //global toast message if something goes wrong
+    fun errorToast() {
+        Toast.makeText(this, "oops, something went wrong. Please try again", Toast.LENGTH_LONG).show()
+        enableSpinner(false)
+    }
+
+    //enabling spinner on load
+    fun enableSpinner(enable : Boolean) {
+        if (enable) {
+            loginSpinner.visibility = View.VISIBLE
+
+        } else {
+            loginSpinner.visibility = View.INVISIBLE
+
+        }
+        loginLoginBtn.isEnabled = !enable
+        loginLoginBtn.isEnabled = !enable
+    }
+
+    //function to hide keyboard
+    fun hideKeyboard() {
+        val inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+        if (inputManager.isAcceptingText) {
+            inputManager.hideSoftInputFromWindow(currentFocus.windowToken, 0)
+        }
+    }
 
 
 }
