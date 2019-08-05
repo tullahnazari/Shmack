ddb670524738558059591c37b5c8e874d29b471b
Mr. Tee <tullah.nazari@gmail.com>
2019-08-05 08:00:50 -0400

    changing package name

---
+++ app/src/main/java/com/tullahnazari/shmack/Utilities/Constants.kt
@@ -0,0 +1,14 @@
+package com.tullahnazari.shmack.Utilities
+
+//API URLS
+const val BASE_URL = "https://schmackchat.herokuapp.com/v1/"
+const val SOCKET_URL = "https://schmackchat.herokuapp.com/"
+const val URL_REGISTER = "${BASE_URL}account/register"
+const val URL_LOGIN = "${BASE_URL}account/login"
+const val URL_CREATE_USER = "${BASE_URL}user/add"
+const val URL_GET_USER = "${BASE_URL}user/byEmail/"
+const val URL_GET_CHANNELS = "${BASE_URL}channel/"
+const val URL_GET_MESSAGES = "${BASE_URL}message/byChannel/"
+
+//Broadcast Constants
+const val BROADCAST_USER_DATA_CHANGE = "BROADCAST_USER_DATA_CHANGE"
